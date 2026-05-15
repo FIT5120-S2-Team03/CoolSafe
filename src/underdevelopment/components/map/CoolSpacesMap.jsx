4d0fbbc9ecb35cce010518bb7f0dce18a5ce08be
Shiying Zhu <szhu0067@student.monash.edu>
2026-05-15 12:08:33 +1000

    Use deployed backend for coolest route

--- src/underdevelopment/components/map/CoolSpacesMap.jsx
+++ src/underdevelopment/components/map/CoolSpacesMap.jsx
@@ -536,7 +536,7 @@ export default function CoolSpacesMap({ selectedCategory, flyTo, showHVI, openVe
 
       setRouteCoords(coords)
 
-      const scoreRes = await fetch('http://127.0.0.1:5001/api/coolest-route', {
+      const scoreRes = await fetch('https://coolsafe.onrender.com/api/coolest-route', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
